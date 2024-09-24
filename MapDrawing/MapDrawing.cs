1e94739ce1a8bb299fd636068dbb7373d09384fa
Maximilian Schmöcker <m.schmoecker@brickfire.eu>
2024-09-24 21:50:18 +0200

    added minimap fog draw

--- MapDrawing/MapDrawing.cs
+++ MapDrawing/MapDrawing.cs
@@ -1,30 +1,27 @@
 ﻿using BepInEx;
+using BepInEx.Logging;
+using HarmonyLib;
 using Jotunn.Entities;
 using Jotunn.Managers;
 using Jotunn.Utils;
 
-namespace MapDrawing
-{
+namespace MapDrawing {
     [BepInPlugin(PluginGUID, PluginName, PluginVersion)]
-    [BepInDependency(Jotunn.Main.ModGuid)]
-    //[NetworkCompatibility(CompatibilityLevel.EveryoneMustHaveMod, VersionStrictness.Minor)]
-    internal class MapDrawing : BaseUnityPlugin
-    {
+    // [BepInDependency(Jotunn.Main.ModGuid)]
+    internal class MapDrawing : BaseUnityPlugin {
         public const string PluginGUID = "com.jotunn.jotunnmodstub";
         public const string PluginName = "MapDrawing";
         public const string PluginVersion = "0.0.1";
-        
-        // Use this class to add your own localization to the game
-        // https://valheim-modding.github.io/Jotunn/tutorials/localization.html
-        public static CustomLocalization Localization = LocalizationManager.Instance.GetLocalization();
 
-        private void Awake()
-        {
-            // Jotunn comes with its own Logger class to provide a consistent Log style for all mods using it
-            Jotunn.Logger.LogInfo("ModStub has landed");
-            
-            // To learn more about Jotunn's features, go to
-            // https://valheim-modding.github.io/Jotunn/tutorials/overview.html
+        public static MapDrawing Instance { get; private set; }
+
+        public static ManualLogSource Log => Instance.Logger;
+
+        private void Awake() {
+            Instance = this;
+
+            Harmony harmony = new Harmony(PluginGUID);
+            harmony.PatchAll();
         }
     }
 }
---
+++ MapDrawing/Patches.cs
@@ -0,0 +1,85 @@
+﻿using HarmonyLib;
+using UnityEngine;
+
+namespace MapDrawing;
+
+[HarmonyPatch]
+public static class Patches {
+    private static Texture2D fogTexture;
+    private static bool[] explored;
+    private static bool overMap;
+
+    [HarmonyPatch(typeof(Minimap), nameof(Minimap.Start)), HarmonyPostfix]
+    public static void DisableFog(Minimap __instance) {
+        overMap = false;
+
+        fogTexture = new Texture2D(__instance.m_textureSize, __instance.m_textureSize, TextureFormat.RGBA32, false);
+        fogTexture.name = "_Minimap m_fogTexture";
+        fogTexture.wrapMode = TextureWrapMode.Clamp;
+
+        explored = new bool[__instance.m_textureSize * __instance.m_textureSize];
+
+        // Color[] colors = new Color[fogTexture.width * fogTexture.height];
+        // for (int i = 0; i < colors.Length; i++) {
+        //     colors[i] = Color.clear;
+        // }
+        //
+        // fogTexture.SetPixels(colors);
+        // fogTexture.Apply();
+
+        __instance.m_mapLargeShader.SetTexture("_FogTex", fogTexture);
+        __instance.m_mapSmallShader.SetTexture("_FogTex", fogTexture);
+        
+        UIInputHandler inputHandler = __instance.m_mapImageLarge.GetComponent<UIInputHandler>();
+        inputHandler.m_onPointerEnter += (handler) => overMap = true;
+        inputHandler.m_onPointerExit += (handler) => overMap = false;
+        inputHandler.m_onRightDown += (handler) => MapDrawing.Log.LogInfo("Right click Down");
+    }
+
+    [HarmonyPatch(typeof(Minimap), nameof(Minimap.Update)), HarmonyPostfix]
+    public static void UpdateFog(Minimap __instance) {
+        if (!overMap)
+            return;
+
+        if (Input.GetMouseButton(1)) {
+            Vector3 worldPoint = __instance.ScreenToWorldPoint(ZInput.mousePosition);
+            Explore(__instance, worldPoint, 10f);
+        }
+    }
+    
+    [HarmonyPatch(typeof(Minimap), nameof(Minimap.OnMapRightClick)), HarmonyPostfix]
+    public static void DrawOnMap(Minimap __instance) {
+        Vector3 worldPoint = __instance.ScreenToWorldPoint(ZInput.mousePosition);
+        // __instance.WorldToMapPoint(worldPoint, out float mx, out float my);
+        Explore(__instance, worldPoint, 10f);
+    }
+
+    public static void Explore(Minimap __instance, Vector3 p, float radius) {
+        int num = (int)Mathf.Ceil(radius / __instance.m_pixelSize);
+        bool flag = false;
+        int px;
+        int py;
+        __instance.WorldToPixel(p, out px, out py);
+        for (int y = py - num; y <= py + num; ++y) {
+            for (int x = px - num; x <= px + num; ++x) {
+                if (x >= 0 && y >= 0 && x < __instance.m_textureSize && y < __instance.m_textureSize && new Vector2(x - px, y - py).magnitude <= (double)num && Explore(__instance, x, y))
+                    flag = true;
+            }
+        }
+
+        if (!flag)
+            return;
+        fogTexture.Apply();
+    }
+
+    public static bool Explore(Minimap __instance, int x, int y) {
+        if (explored[y * __instance.m_textureSize + x])
+            return false;
+        Color pixel = fogTexture.GetPixel(x, y) with {
+            r = 0.0f
+        };
+        fogTexture.SetPixel(x, y, pixel);
+        explored[y * __instance.m_textureSize + x] = true;
+        return true;
+    }
+}
